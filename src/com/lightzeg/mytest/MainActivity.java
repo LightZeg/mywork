3fbdf22dc9ce9673ea03331f01ec780a777810a3
LightZeg <lightzeg@163.com>
2015-09-18 19:58:36 +0800

    Signed-off-by: LightZeg <lightzeg@163.com>

---
+++ src/com/lightzeg/mytest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lightzeg.mytest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
